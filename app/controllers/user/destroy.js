229fe36d1208f9132c4bf36482210f368a71aa50
cvimard <cvimard@socialobjects.co>
2018-06-06 15:35:38 +0200

    feat(user-destroy): remove user

--- app/controllers/user/destroy.js
+++ app/controllers/user/destroy.js
@@ -1,3 +1,6 @@
+// Core
+const mock = require('../../models/get-user.js');
+
 module.exports = class Destroy {
   constructor(app) {
     this.app = app;
@@ -5,12 +8,35 @@ module.exports = class Destroy {
     this.run();
   }
 
+  /**
+   * Middleware
+   */
   middleware () {
+    this.app.get('/user/destroy/:id', (req, res) => {
+      try {
+        if (! req.params || ! req.params.id.length) {
+          res.status(404).json({
+            code: 404,
+            message: 'Not Found'
+          });
+        }
+        delete mock[req.params.id];
+
+        res.status(200).json(mock || {});
+      } catch (e) {
+        console.error(`[ERROR] user/destroy/:id -> ${e}`);
+        res.status(400).json({
+          'code': 400,
+          'message': 'Bad request'
+        });
+      }
+     });
   }
 
+  /**
+   * Run
+   */
   run () {
     this.middleware();
-    console.log('destroy');
   }
 };
-
